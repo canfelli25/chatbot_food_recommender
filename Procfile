d7dc1b1d9572f5594a30830f7dbf42da2bf8a0a0
canfelli25 <fellitacandini@gmail.com>
2019-10-24 01:01:04 +0700

    changing procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: ./entrypoint.sh 
+web: gunicorn -b 0.0.0.0:5001 --workers 20 app:app
+db: ./entrypoint.sh 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+python manage.py db init
+python manage.py db migrate
 python manage.py db upgrade
-gunicorn -b 0.0.0.0:5001 --workers 20 app:app
